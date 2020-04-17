d910e1f2be6fb16484a6f7e0bd8fe19fa2e22d94
Mateusz Wawreszuk <mateusz.wawreszuk@gmail.com>
2020-04-17 14:40:40 +0200

    position book lists

--- library/urls.py
+++ library/urls.py
@@ -4,6 +4,8 @@ from library.views import catalog, user
 
 urlpatterns = [
     path('', catalog.index, name='index'),
+    path('catalog', catalog.index, name='catalog'),
+    path('catalog/<int:position_id>/books/', catalog.books, name='positionBooks'),
     path('user/login/', user.authorize, name='login'),
     path('user/details/', user.user_details, name='userDetails'),
     path('user/logout/', user.logout, name='logout'),
--- library/views/catalog.py
+++ library/views/catalog.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import requests
 from django.http import HttpResponse
 from django.template import loader
@@ -50,5 +52,53 @@ def index(request):
         'title': title,
         'yearFrom': year_from,
         'yearTo': year_to
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def books(request, position_id):
+    try:
+        user = request.session['user']
+        token = user['token']
+    except KeyError:
+        token = ''
+        user = None
+
+    response = requests.get(
+        'http://127.0.0.1:8080/title/getTitle?titleId=' + str(position_id),
+        headers={'Authorization': token}
+    )
+    title = response.json()
+
+    title_books = title['books']
+    now = datetime.now()
+    books_data = []
+    for book in title_books:
+        try:
+            rent_to = datetime.strptime(book['rentTo'], '%Y-%m-%d')
+        except ValueError:
+            rent_to = None
+        except TypeError:
+            rent_to = None
+
+        if rent_to is None:
+            status = 0
+        elif rent_to.date() > now.date():
+            status = 1
+        else:
+            status = 2
+        if rent_to is None:
+            rent_to = '-'
+        else:
+            rent_to = book['rentTo']
+        book_data = {'status': status, 'rentTo': rent_to, 'rentDays': book['rentDays']}
+        books_data.append(book_data)
+    title['books'] = books_data
+
+    template = loader.get_template('catalog/books.html')
+    context = {
+        'title': title,
+        'user': user,
+        'booksCount': len(books_data)
     }
     return HttpResponse(template.render(context, request))
